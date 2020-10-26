d1ecb9b8b26e1b72493becdb12aa0ec0a5d7833a
jadenmounteer <67714386+jadenmounteer@users.noreply.github.com>
2020-10-26 16:04:33 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ import random
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 500
 
-RIFLE_WIDTH = 100
-RIFLE_HEIGHT = 20
+RIFLE_WIDTH = 20 
+RIFLE_HEIGHT = 100 
 RIFLE_COLOR = arcade.color.DARK_RED
 
 BULLET_RADIUS = 3
@@ -53,16 +53,19 @@ class Velocity():
         self.dx = 0
         self.dy = 0
 
-
 # TODO: Fix this class so that all of the flying classes inherit the right things.
 class Flying_Object():
     """
     This class represents a flying object in the game.
     """
     def __init__(self):
+        # Creates a member variable called center and sets it to the Point class.
         self.center = Point()
+        # Creates a member variable called velocity and sets it to the Velocity class.
         self.velocity = Velocity()
+        # Creates a member variable for radius.
         self.radius = 0.0
+        # Creates a member variable called alive and sets it to True.
         self.alive = True
 
 
@@ -95,7 +98,6 @@ class Flying_Object():
         if self.center.y < 0.0 or self.center.y > SCREEN_HEIGHT:
             return False
 
-# TODO: Finish the Bullet class.
 class Bullet(Flying_Object):
     """
     A flying bullet. Child class of the Flying_object class.
@@ -108,25 +110,23 @@ class Bullet(Flying_Object):
         """
         Initializes the bullet.
         """
+        # Uses Super() here in order to override some attributes of the Flying_object class, while keeping others.
         super().__init__()
+
+        # Overrides the radius member variable and gives it the value of BULLET_RADIUS.
         self.radius = BULLET_RADIUS
 
-        # Initializes a bullet starting at (25, 25)
-        self.center.x = 25
-        self.center.y = 25
-
-        # Sounds
-        self.bullet_sound = arcade.load_sound("pepSound1.ogg")
+        # Initializes the bullets at the corner of the screen, inside the barrel.
+        self.center.x = 0
+        self.center.y = 0
 
     def draw(self):
         """
         Draws a bullet to the screen.
-        Renders it aas a filled-in circle.
+        Renders it as a filled-in circle.
         :return: None
         """
-        texture = arcade.load_texture("bullet.png")
-        arcade.draw_texture_rectangle(self.center.x, self.center.y, self.radius*2, self.radius*2, texture)
-        #arcade.draw_circle_filled(self.center.x, self.center.y, self.radius, arcade.color.GOLD)
+        arcade.draw_circle_filled(self.center.x, self.center.y, self.radius, BULLET_COLOR)
 
     def fire(self, angle: float):
         """
@@ -134,25 +134,22 @@ class Bullet(Flying_Object):
         :param angle:
         :return:
         """
+        # --- Inverted ---
         # Angles the bullet horizontally.
-        self.velocity.dx = math.cos(math.radians(angle)) * BULLET_SPEED
+        #self.velocity.dx = math.cos(math.radians(angle)) * BULLET_SPEED
         # Angles the bullet vertically.
-        self.velocity.dy = math.sin(math.radians(angle)) * BULLET_SPEED
+        #self.velocity.dy = math.sin(math.radians(angle)) * BULLET_SPEED
+        # Changes the alive variable to True. Is this necessary?
+        #self.alive = True
+
+        # --- Correct ---
+        # Angles the bullet horizontally.
+        self.velocity.dx = math.sin(math.radians(angle)) * BULLET_SPEED
+        # Angles the bullet vertically.
+        self.velocity.dy = math.cos(math.radians(angle)) * BULLET_SPEED
+        # Changes the alive variable to True. Is this necessary?
         self.alive = True
 
-    #def advance(self):
-        """
-        Moves the bullet through the air.
-        Travels at a 10 pixels/frame at the angle at which
-        it was fired.
-        :return: None
-        """
-        #self.center.x += self.velocity.dx
-        #self.center.y += self.velocity.dy
-
-
-
-# TODO: Finish the Target class.
 class Target(Flying_Object):
     """
     A flying standard target. Child class of the Flying_object class.
@@ -162,34 +159,19 @@ class Target(Flying_Object):
     Adds a hit() method to the class.
     """
     def __init__(self):
+        # Uses Super() here in order to override some attributes of the Flying_object class, while keeping others.
         super().__init__()
 
-        # Target sound
-        self.target_sound = arcade.load_sound("Bomb+1.mp3")
-
-        # Select random location for the Flying object class
-        self.center.x = 0.0
+        # Makes the target spawn anywhere in the left hand corner of the screen.
+        self.center.x = 0.0 #random.randint(0, SCREEN_WIDTH / 2)
         self.center.y = random.randint(SCREEN_HEIGHT // 2, SCREEN_HEIGHT)
 
+        # Overrides the radius member variable and gives it the value of TARGET_RADIUS.
+        self.radius = TARGET_RADIUS
 
-
-    def advance(self):
-        """
-        Makes the target move.
-        :return: None
-        """
-        self.center.x += self.velocity.dx
-        self.center.y += self.velocity.dy
-
-    def is_off_screen(self, screen_width, screen_height):
-        """
-        Checks to see if the bullet is off the screen.
-        :param screen_width:
-        :param screen_height:
-        :return: Boolean
-        """
-        pass
-
+        # Overrides the velocity member variables and gives them the value of the standar target speed.
+        self.velocity.dx = random.uniform(1, 5) # Gives the target a random horizontal velocity between 1 and 5 pixels/frame.
+        self.velocity.dy = random.uniform(-2, 5) # Gives the target a random vertical velocity between -2 and 5 pixesl/frame.
 
     def draw(self):
         """
@@ -198,7 +180,7 @@ class Target(Flying_Object):
         half of the left side of the screen.
         :return:
         """
-        #arcade.draw_circle_filled(self.center.x, self.center.y, self.radius, self.color)
+        arcade.draw_circle_filled(self.center.x, self.center.y, self.radius, TARGET_COLOR)
 
     def hit(self):
         """
@@ -255,6 +237,12 @@ class Game(arcade.Window):
 
         self.rifle = Rifle()
         self.score = 0
+        self.misses = 0
+        self.number_of_bullets_shot = 0
+        self.number_of_targets_shot = 0
+        self.number_of_points_scored = 0
+        self.is_game_over = False
+        
 
         # Creates a list for the bullets.
         self.bullets = []
@@ -262,7 +250,7 @@ class Game(arcade.Window):
         # Creates a list for the targets
         self.targets = []
 
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.SKY_BLUE)
 
     def on_draw(self):
         """
@@ -273,12 +261,16 @@ class Game(arcade.Window):
         # clear the screen to begin drawing
         arcade.start_render()
 
-        # draw each object
-        self.rifle.draw()
+        # ___Draws each object___
+        
 
         # Iterates through the bullets and draws them.
         for bullet in self.bullets:
             bullet.draw()
+        
+        # Draws the rifle after the bullets so that the bullets are only visible once they leave
+        # the barrel.
+        self.rifle.draw()
 
         # Iterates through the targets and draws them.
         for target in self.targets:
@@ -286,12 +278,24 @@ class Game(arcade.Window):
 
         self.draw_score()
 
+        # If the game is over, displays Game over to the screen.
+        if self.is_game_over == True:
+            self.draw_game_over()
+
     def draw_score(self):
         """
         Puts the current score on the screen
         """
         score_text = "Score: {}".format(self.score)
         start_x = 10
+        start_y = SCREEN_HEIGHT - 20
+        arcade.draw_text(score_text, start_x=start_x, start_y=start_y, font_size=12, color=arcade.color.NAVY_BLUE)
+
+        # Draws the player's misses.
+        self.misses = self.number_of_bullets_shot - self.number_of_targets_shot # Calculates the misses.
+        self.misses += self.number_of_points_scored # Makes it so that misses don't decrease as points are accumulated.
+        score_text = "Misses: {}".format(self.misses)
+        start_x = 100
         start_y = SCREEN_HEIGHT - 20
         arcade.draw_text(score_text, start_x=start_x, start_y=start_y, font_size=12, color=arcade.color.NAVY_BLUE)
 
@@ -315,6 +319,10 @@ class Game(arcade.Window):
         # Iterates through the targets and tells them to advance.
         for target in self.targets:
             target.advance()
+        
+        # Checks to see if the player has lost yet.
+        self.is_game_over = self.game_over()
+
 
     def create_target(self):
         """
@@ -327,9 +335,6 @@ class Game(arcade.Window):
         target = Target()
         self.targets.append(target)
 
-
-
-
     def check_collisions(self):
         """
         Checks to see if bullets have hit targets.
@@ -337,11 +342,8 @@ class Game(arcade.Window):
         :return:
         """
 
-        # NOTE: This assumes you named your targets list "targets"
-
-        for bullet in self.bullets:
+        for bullet in self.bullets: 
             for target in self.targets:
-
                 # Make sure they are both alive before checking for a collision
                 if bullet.alive and target.alive:
                     too_close = bullet.radius + target.radius
@@ -351,11 +353,13 @@ class Game(arcade.Window):
                         # its a hit!
                         bullet.alive = False
                         self.score += target.hit()
+                        self.number_of_targets_shot += 1
+                        self.number_of_points_scored += target.hit()
+                    
 
                         # We will wait to remove the dead objects until after we
                         # finish going through the list
-
-        # Now, check for anything that is dead, and remove it
+        # Checks for anything that is dead, and removes it
         self.cleanup_zombies()
 
     def cleanup_zombies(self):
@@ -397,6 +401,7 @@ class Game(arcade.Window):
         bullet.fire(angle)
 
         self.bullets.append(bullet)
+        self.number_of_bullets_shot += 1
 
     def _get_angle_degrees(self, x, y):
         """
@@ -412,6 +417,31 @@ class Game(arcade.Window):
         angle_degrees = math.degrees(angle_radians)
 
         return angle_degrees
+    
+    def game_over(self):
+        """
+        Checks to see if the player loses the game.
+        """
+        if self.misses >= 10:
+            # The game ends. Display game over to the screen.
+            return True
+        else:
+            return False
+            
+    
+    def draw_game_over(self):
+        """
+        Draws GAME OVER to the screen.
+        """
+        game_over_text = "GAME OVER.".format(self.score)
+        start_x = 10
+        start_y = SCREEN_HEIGHT / 2
+        arcade.draw_text(game_over_text, start_x=start_x, start_y=start_y, font_size=50, color=arcade.color.NAVY_BLUE)
+        score_text = "You scored: {} points.".format(self.score)
+        start_x_2 = 10
+        start_y_2 = SCREEN_HEIGHT / 3
+        arcade.draw_text(score_text, start_x=start_x_2, start_y=start_y_2, font_size=50, color=arcade.color.NAVY_BLUE)
+
 
 # Creates the game and starts it going
 window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, "Awesome Skeet")
